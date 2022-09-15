d29f599bdf8d1d01ebdab9fe8adaebe2e6a808fe
tritz18 <61156565+tritz18@users.noreply.github.com>
2022-09-15 16:09:14 -0400

    Simple_Text_Output

--- R_Shiny/app.R
+++ R_Shiny/app.R
@@ -1,19 +1,11 @@
 ui<- fluidPage(
-  selectInput("dataset", label="Dataset", choices=ls(package:datasets)),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
+  textInput("name", "whats your name?", value=NA),
+ textOutput("greeting")
 )
 
 server<-function(input, output, session) {
-    dataset<- reactive({
-      get(input$dataset, "package:datasets")
-  })
-  
-  output$summary<- renderPrint({
-    summary(dataset())
-  })
-  output$table<- renderTable({
-    dataset()
+  output$greeting<- renderText({
+    paste0("Hello", input$name)
   })
 }
 shinyApp(ui,server)
